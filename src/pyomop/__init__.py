7fd68300f1dbe5ac28545b5ce3b3e3592b83870b
Bell Eapen <github@gulfdoctor.net>
2020-05-03 10:47:47 -0400

    refactor: add init and main

--- src/pyomop/__init__.py
+++ src/pyomop/__init__.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from pkg_resources import get_distribution, DistributionNotFound
 
+from engine_factory import CdmEngineFactory
+from cdm6_tables import metadata
+
 try:
     # Change here if project is renamed and does not equal the package name
     dist_name = __name__
