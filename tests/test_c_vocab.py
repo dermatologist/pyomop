ac9295b8a3c37e23d614872a9751d50c0917150f
beapen <github@gulfdoctor.net>
2020-10-18 23:39:56 +0000

    test: Add sample files for test. launch for debug

--- tests/test_c_vocab.py
+++ tests/test_c_vocab.py
@@ -13,6 +13,6 @@ def test_vocab(pyomop_fixture, capsys):
     #     print(x)
     # setters are called like this
     vocab.concept_id = 45956935 # 'C0020538' Hypertension
-    assert(vocab.concept_name == 'Sibutramine hydrochloride')
+    assert(vocab.concept_name == 'xxxx xxxxxxx')
 
 
