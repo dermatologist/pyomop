24dd7fe3254c8bc541339bfc8ecb0cb183093f5a
Bell Eapen <github@gulfdoctor.net>
2023-11-11 23:24:54 -0500

    Add fixtures for Pyomop and metadata in tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,4 +7,16 @@
     https://pytest.org/latest/plugins.html
 """
 
-# import pytest
+import asyncio
+import pytest
+
+@pytest.fixture
+def pyomop_fixture():
+    from src.pyomop import CdmEngineFactory
+    cdm = CdmEngineFactory()
+    return cdm
+
+@pytest.fixture
+def metadata_fixture():
+    from src.pyomop import metadata
+    return metadata
--- tests/test_a_pyomop.py
+++ tests/test_a_pyomop.py
@@ -1,17 +1,6 @@
 import asyncio
 import pytest
 
-@pytest.fixture
-def pyomop_fixture():
-    from src.pyomop import CdmEngineFactory
-    cdm = CdmEngineFactory()
-    return cdm
-
-@pytest.fixture
-def metadata_fixture():
-    from src.pyomop import metadata
-    return metadata
-
 @staticmethod
 @pytest.mark.asyncio
 def test_create_db(pyomop_fixture, metadata_fixture, capsys):
