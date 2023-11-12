a53cf3ed6d837b7d98837b9d7950cae4707bbdbe
Bell Eapen <github@gulfdoctor.net>
2023-11-12 17:11:44 -0500

    Refactored test_create_db to test_create_cohort and added comments and assertions

--- tests/test_a_pyomop.py
+++ tests/test_a_pyomop.py
@@ -3,8 +3,9 @@ import pytest
 
 @staticmethod
 @pytest.mark.asyncio
-def test_create_db(pyomop_fixture, metadata_fixture, capsys):
+def test_create_cohort(pyomop_fixture, metadata_fixture, capsys):
     engine = pyomop_fixture.engine
+    # create tables
     asyncio.run(pyomop_fixture.init_models(metadata_fixture))
     asyncio.run(create_cohort(pyomop_fixture, engine))
 
@@ -14,6 +15,7 @@ async def create_cohort(pyomop_fixture,engine):
     import datetime
     from sqlalchemy.future import select
 
+    # Add a cohort
     async with pyomop_fixture.session() as session:
         async with session.begin():
             session.add(Cohort(cohort_definition_id=2, subject_id=100,
@@ -21,13 +23,18 @@ async def create_cohort(pyomop_fixture,engine):
                 cohort_start_date=datetime.datetime.now()))
         await session.commit()
 
+    # Query the cohort
     stmt = select(Cohort).where(Cohort.subject_id == 100)
     result = await session.execute(stmt)
-
     for row in result.scalars():
         print(row)
         assert row.subject_id == 100
 
-    await session.commit()
+    # Query the cohort pattern 2
+    cohort = await session.get(Cohort, 1)
+    print(cohort)
+    assert cohort.subject_id == 100
+
+    await session.close()
     await engine.dispose()
 
