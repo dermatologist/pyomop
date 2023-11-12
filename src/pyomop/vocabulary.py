6ab7ef439b6f2e2d45f2f1d8a49fc9252dec2b12
Bell Eapen <github@gulfdoctor.net>
2023-11-12 18:13:29 -0500

    Add async functionality to CdmVocabulary class

--- src/pyomop/vocabulary.py
+++ src/pyomop/vocabulary.py
@@ -1,7 +1,7 @@
 from . import CdmEngineFactory
 import pandas as pd
 from .cdm6_tables import Concept
-
+import asyncio
 class CdmVocabulary(object):
     def __init__(self, cdm):
         self._concept_id = 0
@@ -36,22 +36,33 @@ class CdmVocabulary(object):
     @concept_id.setter
     def concept_id(self, concept_id):
         self._concept_id = concept_id
-        _concept = self._cdm.session.query(Concept).filter_by(concept_id=concept_id).one()
+        # _concept = self._cdm.session.query(Concept).filter_by(concept_id=concept_id).one()
+        _concept = asyncio.run(self.get_concept(concept_id))
         self._concept_name = _concept.concept_name
         self._domain_id = _concept.domain_id
         self._vocabulary_id = _concept.vocabulary_id
         self._concept_class_id = _concept.concept_class_id
         self._concept_code = _concept.concept_code
 
+    async def get_concept(self, concept_id):
+        stmt = self._cdm.session.query(Concept).filter_by(concept_id=concept_id).one()
+        _concept = await self._cdm.session.execute(stmt)
+        return _concept
+
+    async def get_concept_by_code(self, concept_code, vocabulary_id):
+        stmt = self._cdm.session.query(Concept).filter_by(concept_code=concept_code) \
+            .filter_by(vocabulary_id=vocabulary_id).one()
+        _concept = await self._cdm.session.execute(stmt)
+        return _concept
+
     def set_concept(self, concept_code, vocabulary_id=None):
         self._concept_code = concept_code
         try:
             if vocabulary_id is not None:
                 self._vocabulary_id = vocabulary_id
-                _concept = self._cdm.session.query(Concept).filter_by(concept_code=concept_code) \
-                    .filter_by(vocabulary_id=vocabulary_id).one()
+                _concept = asyncio.run(self.get_concept_by_code(concept_code, vocabulary_id))
             else:
-                _concept = self._cdm.session.query(Concept).filter_by(concept_code=concept_code).one()
+                _concept = asyncio.run(self.get_concept_by_code(concept_code))
                 self._vocabulary_id = _concept.vocabulary_id
 
             self._concept_name = _concept.concept_name
@@ -68,23 +79,32 @@ class CdmVocabulary(object):
         if sample < 1000: # nrows=sample
             try:
                 df = pd.read_csv(folder + '/DRUG_STRENGTH.csv', sep='\t', nrows=sample, on_bad_lines='skip')
-                df.to_sql('drug_strength', con=self._engine, if_exists = 'replace')
+                asyncio.run(self.write_vocab(df, 'drug_strength', 'replace'))
+                # df.to_sql('drug_strength', con=self._engine, if_exists = 'replace')
                 df = pd.read_csv(folder + '/CONCEPT.csv', sep='\t', nrows=sample, on_bad_lines='skip')
-                df.to_sql('concept', con=self._engine, if_exists = 'replace')
+                asyncio.run(self.write_vocab(df, 'concept', 'replace'))
+                # df.to_sql('concept', con=self._engine, if_exists = 'replace')
                 df = pd.read_csv(folder + '/CONCEPT_RELATIONSHIP.csv', sep='\t', nrows=sample, on_bad_lines='skip')
-                df.to_sql('concept_relationship', con=self._engine, if_exists = 'replace')
+                asyncio.run(self.write_vocab(df, 'concept_relationship', 'replace'))
+                # df.to_sql('concept_relationship', con=self._engine, if_exists = 'replace')
                 df = pd.read_csv(folder + '/CONCEPT_ANCESTOR.csv', sep='\t', nrows=sample, on_bad_lines='skip')
-                df.to_sql('concept_ancester', con=self._engine, if_exists = 'replace')
+                asyncio.run(self.write_vocab(df, 'concept_ancester', 'replace'))
+                # df.to_sql('concept_ancester', con=self._engine, if_exists = 'replace')
                 df = pd.read_csv(folder + '/CONCEPT_SYNONYM.csv', sep='\t', nrows=sample, on_bad_lines='skip')
-                df.to_sql('concept_synonym', con=self._engine, if_exists = 'replace')
+                asyncio.run(self.write_vocab(df, 'concept_synonym', 'replace'))
+                # df.to_sql('concept_synonym', con=self._engine, if_exists = 'replace')
                 df = pd.read_csv(folder + '/VOCABULARY.csv', sep='\t', nrows=sample, on_bad_lines='skip')
-                df.to_sql('vocabulary', con=self._engine, if_exists = 'replace')
+                asyncio.run(self.write_vocab(df, 'vocabulary', 'replace'))
+                # df.to_sql('vocabulary', con=self._engine, if_exists = 'replace')
                 df = pd.read_csv(folder + '/RELATIONSHIP.csv', sep='\t', nrows=sample, on_bad_lines='skip')
-                df.to_sql('relationship', con=self._engine, if_exists = 'replace')
+                asyncio.run(self.write_vocab(df, 'relationship', 'replace'))
+                # df.to_sql('relationship', con=self._engine, if_exists = 'replace')
                 df = pd.read_csv(folder + '/CONCEPT_CLASS.csv', sep='\t', nrows=sample, on_bad_lines='skip')
-                df.to_sql('concept_class', con=self._engine, if_exists = 'replace')
+                asyncio.run(self.write_vocab(df, 'concept_class', 'replace'))
+                # df.to_sql('concept_class', con=self._engine, if_exists = 'replace')
                 df = pd.read_csv(folder + '/DOMAIN.csv', sep='\t', nrows=sample, on_bad_lines='skip')
-                df.to_sql('domain', con=self._engine, if_exists = 'replace')
+                asyncio.run(self.write_vocab(df, 'domain', 'replace'))
+                # df.to_sql('domain', con=self._engine, if_exists = 'replace')
             except ValueError:
                 print("Oops!  Could not write vocabulary")
         else:
@@ -110,3 +130,14 @@ class CdmVocabulary(object):
             except ValueError:
                 print("Oops!  Could not write vocabulary")
 
+    async def write_vocab(self, df, table, if_exists='replace'):
+        # async with engine.begin() as conn:
+        #     await conn.run_sync(df.to_sql, table, con=conn, if_exists=if_exists)
+        async with self._cdm.session() as session:
+            conn = await session.connection()
+            await conn.run_sync(
+                lambda sync_conn: df.to_sql(
+                    table,
+                    if_exists=if_exists,
+                ),
+            )
--- tests/test_b_create_vocab.py
+++ tests/test_b_create_vocab.py
@@ -1,19 +1,38 @@
+import asyncio
 import pytest
-import os
-
-@pytest.fixture
-def pyomop_fixture():
-    from src.pyomop import CdmEngineFactory
-    cdm = CdmEngineFactory()
-    return cdm
-
-@pytest.fixture
-def metadata_fixture():
-    from src.pyomop import metadata
-    return metadata
 
 def test_create_vocab(pyomop_fixture, metadata_fixture, capsys):
+    engine = pyomop_fixture.engine
+    create_vocab(pyomop_fixture, engine)
+
+def create_vocab(pyomop_fixture,engine):
     from src.pyomop import CdmVocabulary
     vocab = CdmVocabulary(pyomop_fixture)
     vocab.create_vocab('tests', 10)
     print("Done")
+
+
+
+
+
+
+
+# import pytest
+# import os
+
+# @pytest.fixture
+# def pyomop_fixture():
+#     from src.pyomop import CdmEngineFactory
+#     cdm = CdmEngineFactory()
+#     return cdm
+
+# @pytest.fixture
+# def metadata_fixture():
+#     from src.pyomop import metadata
+#     return metadata
+
+# def test_create_vocab(pyomop_fixture, metadata_fixture, capsys):
+#     from src.pyomop import CdmVocabulary
+#     vocab = CdmVocabulary(pyomop_fixture)
+#     vocab.create_vocab('tests', 10)
+#     print("Done")
