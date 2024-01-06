9dd8b77aa9fa550866f386ba323acabf7a66079e
Bell Eapen <github@gulfdoctor.net>
2024-01-05 20:53:58 -0500

    Refactor test_e_llm_query.py and add datetime import

--- tests/test_e_llm_query.py
+++ tests/test_e_llm_query.py
@@ -1,8 +1,7 @@
 import asyncio
 import pytest
-from llama_index.llms import Vertex
-from src.pyomop.llm_engine import CDMDatabase
 import os
+import datetime
 
 @staticmethod
 def test_create_cohort(pyomop_fixture, metadata_fixture, capsys):
@@ -13,19 +12,19 @@ def test_create_cohort(pyomop_fixture, metadata_fixture, capsys):
 
 
 async def create_llm_query(pyomop_fixture,engine):
-    from src.pyomop import Cohort, CdmLLMQuery
-    import datetime
+    response = "I'm running in CI with no LLM"
+    if "CI" not in os.environ or not os.environ["CI"] or "GITHUB_RUN_ID" not in os.environ or "DOCSDIR" not in os.environ:
+        from src.pyomop import Cohort, CdmLLMQuery
+        from llama_index.llms import Vertex
+        from src.pyomop.llm_engine import CDMDatabase
+        # Add a cohort
+        async with pyomop_fixture.session() as session:
+            async with session.begin():
+                session.add(Cohort(cohort_definition_id=2, subject_id=100,
+                    cohort_end_date=datetime.datetime.now(),
+                    cohort_start_date=datetime.datetime.now()))
+            await session.commit()
 
-    # Add a cohort
-    async with pyomop_fixture.session() as session:
-        async with session.begin():
-            session.add(Cohort(cohort_definition_id=2, subject_id=100,
-                cohort_end_date=datetime.datetime.now(),
-                cohort_start_date=datetime.datetime.now()))
-        await session.commit()
-
-        response = "I'm running in CI with no LLM"
-        if "CI" not in os.environ or not os.environ["CI"] or "GITHUB_RUN_ID" not in os.environ or "DOCSDIR" not in os.environ:
             # Use any LLM that llama_index supports
             llm = Vertex(
                 model="chat-bison",
@@ -36,6 +35,7 @@ async def create_llm_query(pyomop_fixture,engine):
             query_engine = CdmLLMQuery(sql_database, llm=llm)
 
             response  = query_engine.query("Show each in table cohort with a subject id of 100?")
+    ## If we are running in CI, we don't have access to the LLM
     print(response)
     await session.close()
     await engine.dispose()
